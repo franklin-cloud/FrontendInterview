0ca1022896f3f88a4eb90511d023dbbbaee8bf6a
Franklin <fuhb_0309@163.com>
2024-11-20 22:05:25 +0800

    文件目录整理

--- getFile.js
+++ getFile.js
@@ -4,30 +4,31 @@ const path = require("path");
 const resolvePath = (filePath) => {
   return path.join(__dirname, filePath);
 };
-
-const targetPath = resolvePath("src");
-const outputDirInfoPath = resolvePath("dist/dirInfo.js");
+// 目标目录
+const targetPath = resolvePath("articles");
+// 输出目录
+const outputDir = "D:/MySpace/Work/wx_interview/articles";
+// 所有文件名
 const allFileNames = [];
 
 // 获取目录
-const getTargetDir = (dirPath) => {
+const getAllDirs = (dirPath) => {
   const dirs = fs.readdirSync(dirPath);
   const filterDirs = dirs.filter((dirItem) => {
     return /^(\d){2}\./.test(dirItem);
   });
   return filterDirs;
 };
+
 // 写入文件
 const writeJsFile = (path, content) => {
   const fileContent = `export default ${JSON.stringify(content)}`;
-  fs.writeFile(path, fileContent, (err, data) => {
-    console.log("写入成功:", path);
-  });
+  fs.writeFileSync(path, fileContent, "utf-8");
+  console.log("写入成功:", path);
 };
 
 // 获取的文件
 const getFiles = (dirPath) => {
-  // src/01.浏览器/所有文件
   const allFiles = fs.readdirSync(dirPath);
   // 过滤掉images文件夹
   const files = allFiles.filter((file) => {
@@ -35,43 +36,64 @@ const getFiles = (dirPath) => {
   });
 
   files.forEach((file) => {
-    const subPath = dirPath + `/${file}`;
+    const subPath = `${dirPath}/${file}`;
     const fileState = fs.statSync(subPath);
     if (fileState.isFile()) {
-      allFileNames.push(subPath.replace(resolvePath("src") + "\\", ""));
-      // 输出文件内容
+      // 移除绝对路径
+      const fileName = subPath.replace(`${targetPath}/`, "");
+      // console.log("fileName:", fileName);
+      allFileNames.push(fileName);
+      // 读取文件内容
       const contentStr = fs.readFileSync(subPath, "utf-8");
-      // 获取输出路径
-      const subPathArr = subPath.split("src\\");
-      let outputPath = subPathArr[0] + "src\\" + subPathArr[1].replace(/\//g, "=");
-      outputPath = outputPath.replace("src", "dist").replace(/\.(md|html)$/, ".js");
-      writeJsFile(outputPath, contentStr);
+      // 输出路径: outputDir目录下平铺展示
+      const fileOutputPath = outputDir + "/" + fileName.replace(/\//g, "=").replace(/\.(md|html)$/, ".js");
+      writeJsFile(fileOutputPath, contentStr);
     } else if (fileState.isDirectory()) {
       getFiles(subPath);
-    } else {
-      console.log("未知类型");
     }
   });
 };
 
 // 获取目录下所有文件
-const getAllFiles = (targetDirs) => {
-  const exitsDist = fs.existsSync(resolvePath("/dist"));
+const getAllFiles = (allDirs) => {
+  const exitsDist = fs.existsSync(outputDir);
   if (!exitsDist) {
-    fs.mkdirSync(resolvePath("/dist"));
+    fs.mkdirSync(outputDir);
   }
-  // 写入一级目录信息
-  writeJsFile(outputDirInfoPath, targetDirs);
-
   // 读取所有文件内容
-  targetDirs.forEach((dirItamPath) => {
-    const dirPath = resolvePath("/src/" + dirItamPath);
+  allDirs.forEach((dirItamPath) => {
+    const dirPath = `${targetPath}/${dirItamPath}`;
     getFiles(dirPath);
   });
 };
 
-const targetDirs = getTargetDir(targetPath);
+const allDirs = getAllDirs(targetPath);
+getAllFiles(allDirs);
 
-getAllFiles(targetDirs);
+// 获取文章列表
+const getArticleList = (allFileNames) => {
+  const data = {};
+  allFileNames.forEach((filePath) => {
+    const pathArr = filePath.split("/");
+    const dir = pathArr[0];
+    const subDir = pathArr[1];
+    const fileName = pathArr[pathArr.length - 1];
+    if (pathArr.length === 2) {
+      if (!data[dir]) {
+        data[dir] = [];
+      }
+      data[dir].push(fileName);
+    } else if (pathArr.length === 3) {
+      if (!data[dir]) {
+        data[dir] = {};
+      }
+      if (!data[dir][subDir]) {
+        data[dir][subDir] = [];
+      }
+      data[dir][subDir].push(fileName);
+    }
+  });
+  writeJsFile(outputDir + "/articleList.js", data);
+};
 
-writeJsFile(resolvePath("/dist/allFileNames.js"), allFileNames);
+getArticleList(allFileNames);
